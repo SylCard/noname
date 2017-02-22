ea9df963ac3b84880682e44d1df3cf377e454a34
fudgemcgroobs <tudor.cismarescu@gmail.com>
2017-02-22 21:16:35 +0000

    Made mainController global

--- Frontend/js/mainController.js
+++ Frontend/js/mainController.js
@@ -1,3 +1,11 @@
 app.controller('mainController', ['$scope', function($scope) {
     //main page content controller properties or functions
+    $scope.anomalies = [];
+
+    $scope.addAnomaly = function(data) {
+        $scope.anomalies.push(data);
+    };
+    $scope.removeAnomaly = function(index) {
+        $scope.anomalies.splice(index, 1);
+    };
 }]);
--- Frontend/js/navController.js
+++
@@ -1,4 +0,0 @@
-app.controller('navController', ['$scope', function($scope) {
-    $scope.title = "some title";
-    $scope.numAlerts = 0;
-}]);
