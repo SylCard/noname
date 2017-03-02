d1c076c03bee261b4c93c2691a2b182f0d479d61
fudgemcgroobs <tudor.cismarescu@gmail.com>
2017-03-02 00:05:53 +0000

    commented around Angular bits

--- Frontend/js/Angular/mainController.js
+++ Frontend/js/Angular/mainController.js
@@ -1,5 +1,7 @@
 app.controller('mainController', ['$scope', function($scope) {
     //main page content controller properties or functions
+
+    //possible functions to call when alerts come in/need to be deleted aafter sending response
     $scope.addAnomaly = function(data) {
         $scope.anomalies.push(data);
     };
--- Frontend/js/Angular/spikeApp.js
+++ Frontend/js/Angular/spikeApp.js
@@ -1,10 +1,11 @@
 var app = angular.module("spikeApp", []);
 
 app.run(function($rootScope) {
+    //array used store alerts and generate alert boxes
     $rootScope.anomalies = [
         {
             AnomalyID: 1,
-            type: 'FatFinger',
+            type: 'FatFinger',  //needed to identify which alert template to use when generating html
             stock: {
                 symbol: 'SYM',
                 buyer: 'silver@pumpanddump.me',
