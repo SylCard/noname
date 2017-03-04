07a08083093222d742c04133bf088f412b534d7f
fudgemcgroobs <tudor.cismarescu@gmail.com>
2017-03-04 14:02:48 +0000

    deleted my failed socket.io attempt

--- Frontend/js/SocketIO/socket.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('spikeApp')
-//Here LoopBackAuth service must be provided as argument for authenticating the user
-.factory('socket', function(LoopBackAuth){
-    //Creating connection with server
-    var socket = io.connect('http://localhost:3000');
-});
