172c04d19d7e5f2e85687b04dbb624da6401a379
fudgemcgroobs <tudor.cismarescu@gmail.com>
2017-03-04 03:12:50 +0000

    changed JSON templates
    angular expressions do not like '_'

--- Frontend/js/Angular/spikeApp.js
+++ Frontend/js/Angular/spikeApp.js
@@ -3,6 +3,15 @@ var app = angular.module("spikeApp", []);
 app.run(function($rootScope) {
     //array used store alerts and generate alert boxes
     $rootScope.anomalies = [{
+            AnomalyID: 2,
+            type: 'VolumeSpike',
+            symbol: 'SYM',
+            yaxis1: [0, 0, 0, 0],
+            yaxis2: [0, 0, 0, 0],
+            time_begin: 1488593766466,
+            period_len: 30000
+        },
+        {
             AnomalyID: 1,
             type: 'FatFinger', //needed to identify which alert template to use when generating html
             stock: {
@@ -15,15 +24,6 @@ app.run(function($rootScope) {
             },
             severity: 50,
             RMA: 1000
-        },
-        {
-            AnomalyID: 2,
-            type: 'VolumeSpike',
-            symbol: 'SYM',
-            yaxis1: [0, 0, 0, 0],
-            yaxis2: [0, 0, 0, 0],
-            time_begin: 1488593766466,
-            period_len: 30000
         }
     ];
 
