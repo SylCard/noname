6678d420e0c1d880efee5de426ac2d6d3a4c5820
fudgemcgroobs <tudor.cismarescu@gmail.com>
2017-03-06 00:45:32 +0000

    removed extra code. added console log to test

--- Frontend/js/Angular/mainController.js
+++ Frontend/js/Angular/mainController.js
@@ -1,10 +1,9 @@
 app.controller('mainController', ['$scope', function($scope) {
     //main page content controller properties or functions
     $scope.drop = function(id) {
-        $(document).ready(function(e) {
-            $('#dropdown' + id).on('click', function() { // !!! replace number with {{alertId}} etc.
-                $('.dropdownwrap' + id).slideToggle();
-            });
+        console.log(id);
+        $('#dropdown' + id).on('click', function() { // !!! replace number with {{alertId}} etc.
+            $('.dropdownwrap' + id).slideToggle();
         });
     };
 }]);
