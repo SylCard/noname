34fa592caf907f7a01d32a16bc04ae00dd51ad25
fudgemcgroobs <tudor.cismarescu@gmail.com>
2017-03-06 00:42:06 +0000

    Moved dropdown function to controller

--- Frontend/js/Angular/mainController.js
+++ Frontend/js/Angular/mainController.js
@@ -1,4 +1,10 @@
 app.controller('mainController', ['$scope', function($scope) {
     //main page content controller properties or functions
-
+    $scope.drop = function(id) {
+        $(document).ready(function(e) {
+            $('#dropdown' + id).on('click', function() { // !!! replace number with {{alertId}} etc.
+                $('.dropdownwrap' + id).slideToggle();
+            });
+        });
+    };
 }]);
