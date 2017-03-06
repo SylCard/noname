72f7369f07d2b73ba9f0b20b8cbda52facb97c0f
fudgemcgroobs <tudor.cismarescu@gmail.com>
2017-03-06 00:49:41 +0000

    stripped down dropdown code

--- Frontend/js/Angular/mainController.js
+++ Frontend/js/Angular/mainController.js
@@ -1,9 +1,7 @@
 app.controller('mainController', ['$scope', function($scope) {
     //main page content controller properties or functions
     $scope.drop = function(id) {
-        console.log(id);
-        $('#dropdown' + id).on('click', function() { // !!! replace number with {{alertId}} etc.
-            $('.dropdownwrap' + id).slideToggle();
-        });
+        alert(id);
+        $('.dropdownwrap' + id).slideToggle();
     };
 }]);
