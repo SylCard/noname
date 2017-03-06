389667a8e36d743fcbcf3f409e7a9a4d992e1552
fudgemcgroobs <tudor.cismarescu@gmail.com>
2017-03-06 23:49:28 +0000

    now actual code in the 'drop' function
    omg i think it's working

--- Frontend/js/Angular/mainController.js
+++ Frontend/js/Angular/mainController.js
@@ -1,7 +1,7 @@
 app.controller('mainController', ['$scope', function($scope) {
     //main page content controller properties or functions
     $scope.drop = function(id) {
-        alert(id);
-        //$('.dropdownwrap' + id).slideToggle();
+        //alert(id);
+        $('.dropdownwrap' + id).slideToggle();
     };
 }]);
